0ff94653658a68f472220748ed3a5a6fe8f38b2a
wbyjxsh <108548167+wbyjxsh@users.noreply.github.com>
2023-11-28 14:09:01 -0500

    updated app

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ import torch
 import torch.nn as nn
 from torchvision import transforms
 from efficientnet_pytorch import EfficientNet
-import numpy as np
 import PIL
-
-
+import numpy as np
+import stainNorm_Macenko
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 class ClassifyModel:
     def __init__(self):
         self.model = None
@@ -33,10 +34,12 @@ class ClassifyModel:
         self.checkpoint_val = None
         self.checkpoint_str = None
         self.checkpoint_tum = None
-        # To be changed
-        self.val_acc = 0.9568245125348189
-        self.str_acc = 0.7648456057007126
-        self.tum_acc = 0.9878345498783455
+        self.val_acc = 0.9638
+        self.val_prec = 0.94
+        self.str_recall = 0.8242
+        self.tum_recall = 0.9984
+        self.str_prec = 0.82
+        self.tum_prec = 0.84
         
 
     def load(self):
@@ -50,45 +53,75 @@ class ClassifyModel:
 
         # Detect if we have a GPU available
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        # To be changed
-        self.checkpoint_val = torch.load("model_result_112_b3_epoch_4_20231127_052702.pth", map_location=device)
-        self.checkpoint_str = torch.load("model_result_112_b3_epoch_1_20231127_184058.pth", map_location=device)
-        self.checkpoint_tum = torch.load("model_result_112_b3_epoch_4_20231127_185353.pth", map_location=device)
+        
+        self.checkpoint_val = torch.load("model_result_112_b1_epoch_11_20231126_225903.pth", map_location=device)
+        self.checkpoint_str = torch.load("model_result_56_b4_epoch_5_20231127_033430.pth", map_location=device)
+        self.checkpoint_tum = torch.load("model_result_112_b2_epoch_6_20231127_043043.pth", map_location=device)
         
          
 
     def predict(self, imgs):
         # Initialize the model for multi-GPU if available
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        model = EfficientNet.from_pretrained('efficientnet-b3')
-        num_ftrs = model._fc.in_features
-        model._fc = nn.Linear(num_ftrs, 9)
-        model = nn.DataParallel(model)
-        model.to(device)
+        
+        transform_str = transforms.Compose([
+            transforms.Resize((56, 56)),  # Resize the image
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
 
         inputs = self.transform(imgs).unsqueeze(0)
+        inputs_str = transform_str(imgs).unsqueeze(0)
         inputs = inputs.to(device)
-        model.load_state_dict(self.checkpoint_val)
-        model.eval()
+        
+        model_val = EfficientNet.from_pretrained('efficientnet-b1')
+        num_ftrs = model_val._fc.in_features
+        model_val._fc = nn.Linear(num_ftrs, 9)
+        model_val.to(device)
+        model_val.load_state_dict(self.checkpoint_val)
+        model_val.eval()
+        
         with torch.no_grad():
-            outputs = model(inputs)
+            outputs = model_val(inputs)
             _, preds_val = torch.max(outputs, 1)
-        model.load_state_dict(self.checkpoint_str)
-        model.eval()
+        
+        model_tum = EfficientNet.from_pretrained('efficientnet-b2')
+        num_ftrs = model_tum._fc.in_features
+        model_tum._fc = nn.Linear(num_ftrs, 9)
+        model_tum = nn.DataParallel(model_tum)
+        model_tum.to(device)
+        model_tum.load_state_dict(self.checkpoint_tum)
+        model_tum.eval()
         with torch.no_grad():
-            outputs_str = model(inputs)
-            _, preds_str = torch.max(outputs_str, 1)
-        model.load_state_dict(self.checkpoint_tum)
-        model.eval()
-        with torch.no_grad():
-            outputs_tum = model(inputs)
+            outputs_tum = model_tum(inputs)
             _, preds_tum = torch.max(outputs_tum, 1)
+        
+        model_str = EfficientNet.from_pretrained('efficientnet-b4')
+        num_ftrs = model_str._fc.in_features
+        model_str._fc = nn.Linear(num_ftrs, 9)
+        model_str = nn.DataParallel(model_str)
+        model_str.to(device)
+        model_str.load_state_dict(self.checkpoint_str)
+        model_str.eval()
+        with torch.no_grad():
+            outputs_str = model_str(inputs_str)
+            _, preds_str = torch.max(outputs_str, 1)
+        
         if preds_tum[0] == 8:
-            return self.tag2class[preds_tum.cpu().numpy()[0]], self.tum_acc
+            return self.tag2class[preds_tum.cpu().numpy()[0]], self.tum_recall, self.tum_prec
         elif preds_str[0] == 7:
-            return self.tag2class[preds_str.cpu().numpy()[0]], self.str_acc
+            return self.tag2class[preds_str.cpu().numpy()[0]], self.str_recall, self.str_prec
         else: 
-            return self.tag2class[preds_val.cpu().numpy()[0]], self.val_acc
+            return self.tag2class[preds_val.cpu().numpy()[0]], self.val_acc, self.val_prec
+
+def Macenko(image, target_image):
+    image = np.array(image)
+    target_image = np.array(target_image)
+    n = stainNorm_Macenko.Normalizer()
+    n.fit(target_image)
+    image = n.transform(image)
+    image = PIL.Image.fromarray(image)
+    return image
 
 m = ClassifyModel()
 m.load()
@@ -106,7 +139,8 @@ uploaded_file = st.file_uploader(label=" ", label_visibility='collapsed')
 
 if uploaded_file is not None:
     image = PIL.Image.open(uploaded_file).convert('RGB')
-    # img = np.array(image)
-    tissue_type, confidence = m.predict(image)
-    st.write(f"I think this is **{tissue_type}**(confidence: **{round(float(confidence),4)*100}%**)")
+    target_image = PIL.Image.open("TUM-TCGA-AAMWWECY.tif").convert('RGB')
+    image = Macenko(image, target_image)
+    tissue_type, accuracy, confidence = m.predict(image)
+    st.write(f"I think this is **{tissue_type}** with accuracy: **{accuracy}** and confidence: **{round(float(confidence),4)*100}%**.")
     st.image(image, caption='Uploaded Image.', use_column_width=True)
